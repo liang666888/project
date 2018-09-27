709e19de5e00cdab6763f7b89ef27b96cfa8b809
liang666888 <790743717@qq.com>
2018-09-27 14:52:03 +0800

    提交

--- demo/src/main/java/com/cloud/demo/controller/TestController.java
+++ demo/src/main/java/com/cloud/demo/controller/TestController.java
@@ -4,6 +4,7 @@
 package com.cloud.demo.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @Date 2018年7月27日 下午2:18:29
  * @since JDK 1.8
  */
+@RefreshScope
 @RestController
 public class TestController {
 	
