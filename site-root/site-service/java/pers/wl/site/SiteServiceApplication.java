def35706be38b1f81d305502c9c52f75cc7c1d2b
liang666888 <790743717@qq.com>
2018-10-17 17:04:02 +0800

    工程结构调整

---
+++ site-root/site-service/java/pers/wl/site/SiteServiceApplication.java
@@ -0,0 +1,22 @@
+/**
+ * Copyright © 2017-2018 WL.All Rights Reserved.
+ */
+package pers.wl.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/** 
+ * 描述说明
+ * 初始化启动类
+ * @version V1.0
+ * @author wuliang@https://github.com/liang666888
+ * @Date 2018年10月12日 上午11:36:15
+ * @since JDK 1.8
+ */
+@SpringBootApplication
+public class SiteServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SiteServiceApplication.class, args);
+	}
+}
