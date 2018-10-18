ba07d12050ab412a13c1dd55afb58b8dfea10630
liang666888 <790743717@qq.com>
2018-10-18 14:11:11 +0800

    logback配置提交

--- site-root/site-web/src/main/java/pers/wl/site/web/controller/TestController.java
+++ site-root/site-web/src/main/java/pers/wl/site/web/controller/TestController.java
@@ -3,10 +3,14 @@
  */
 package pers.wl.site.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
 
 import pers.wl.site.model.UserModel;
 import pers.wl.site.web.client.TestControllerClient;
@@ -23,12 +27,22 @@ import pers.wl.site.web.client.TestControllerClient;
 @RequestMapping("/test")
 public class TestController {
 
+	private final static Logger logger = LoggerFactory.getLogger(TestController.class);
+
 	@Autowired
 	TestControllerClient testServiceClient;
 
 	@GetMapping("/sayhi")
 	public String sayHi() {
-		UserModel userModel = testServiceClient.getUser();
+		logger.info("访问sayhi");
+		UserModel userModel = null;
+		try {
+			userModel = testServiceClient.getUser();
+		} catch (Exception e) {
+			logger.error(e.getMessage());
+		}
+
+		logger.info("请求返回[testServiceClient.getUser()]:" + JSON.toJSONString(userModel));
 		return testServiceClient.sayHello() + userModel.getName();
 	}
 }
