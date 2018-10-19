c4f851a5c30fbcb58bd872b4118a3da483eca18f
liang666888 <790743717@qq.com>
2018-10-19 17:42:34 +0800

    创建album-service

--- site-root/album-service/src/main/java/pers/wl/album/AlbumServiceApplication.java
+++ site-root/album-service/src/main/java/pers/wl/album/AlbumServiceApplication.java
@@ -1,26 +1,26 @@
 /**
  * Copyright © 2017-2018 WL.All Rights Reserved.
  */
-package pers.wl.site;
+package pers.wl.album;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.spring4all.swagger.EnableSwagger2Doc;
 
-/** 
+/**
  * 描述说明
- * 初始化启动类
- * 注：@EnableEurekaClient springboot2.0以上可省略
+ * 
  * @version V1.0
  * @author wuliang@https://github.com/liang666888
- * @Date 2018年10月12日 上午11:36:15
+ * @Date 2018年10月19日 下午5:37:31
  * @since JDK 1.8
  */
 @EnableSwagger2Doc
 @SpringBootApplication
-public class SiteServiceApplication {
+public class AlbumServiceApplication {
+
 	public static void main(String[] args) {
-		SpringApplication.run(SiteServiceApplication.class, args);
+		SpringApplication.run(AlbumServiceApplication.class, args);
 	}
 }
